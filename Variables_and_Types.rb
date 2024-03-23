8e83f073097b32627290663552210872d03ba172
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-03-23 11:27:33 +0600

    Variables and Types

---
+++ Variables_and_Types.rb
@@ -0,0 +1,11 @@
+myFirstVariable = 'I made a variable'
+
+puts myFirstVariable
+
+aString = "I'm a string!" 
+aBoolean = true 
+aNumber = 42
+
+puts myString = "I'm programming"
+puts myBoolean = true
+puts myNumber = 5
