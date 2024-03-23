230ef2e60ea63f3cee47ea4a88cda90e0bd7cf40
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-03-23 11:30:41 +0600

    Math

---
+++ Math.rb
@@ -0,0 +1,16 @@
+moduloAnswer=5%2 
+exponentAnswer=2**3 
+
+
+numberOne = 6
+numberTwo = 8
+numberThree = 5
+numberFour = 12
+numberFive = 36
+
+testOne = numberTwo * numberThree #fill in the blank to make this 40
+testTwo = numberFive % numberOne #fill in the blank to make this 0
+testThree = numberFour - numberThree #fill in the blank to make this 7
+if testOne == 40 && testTwo == 0 && testThree == 7
+    puts true
+end
