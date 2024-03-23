26d69956dbe4c26da0789c65cf24ee9c3893720e
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-03-23 11:34:01 +0600

    Strings

---
+++ Strings.rb
@@ -0,0 +1,28 @@
+myFirstString = 'I am a string!' #single quotes
+mySecondString = "Me too!" #double quotes
+
+"Hi!".length #is 3
+
+
+"Hi!".reverse #is !iH
+
+
+"Hi!".upcase #is HI!
+
+
+"Hi!".downcase #is hi!
+
+
+"Hi!".downcase.reverse #is !ih
+
+
+"Happy Birthday!".include?("Happy")
+
+# The above code evaluates to true.
+
+
+
+# Take the given string and use methods to make the code print out "!edoc ma i !ih".
+myString = "Hi! I am code!" #In the next line, use methods to change it.
+myNewString = myString.reverse.downcase
+puts myNewString
