77281a24574992907e56a136e4195b801c308f5e
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-03-23 11:23:41 +0600

    first commit

---
+++ Hello.rb
@@ -0,0 +1,9 @@
+puts "I will be printed to the console!"
+p 'Hello world'
+puts "Hello 
+world!"
+# The difference betwen puts and print is that in the puts, it does not care about any spaces
+ print 'Hello 
+ World !!!'
+
+ # When we use print and if we use space, it will show up in the output
