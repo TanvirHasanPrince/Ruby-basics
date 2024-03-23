b2485bcd51db4611b9582e36f1bad9e99ef144e3
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-03-23 12:08:29 +0600

    Methods

---
+++ Methods.rb
@@ -0,0 +1,11 @@
+def methodname
+    # method code here
+end
+
+# Using the provided code, create a method called say_hi that puts "Hi!" to the console. (Remember to call it!)
+
+def say_hi 
+ puts"Hi!"
+end
+
+say_hi
