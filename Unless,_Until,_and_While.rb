35a25025bfdaa8acd64e1e565167bbecfcbd56f4
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-03-23 12:05:09 +0600

    Unless, Until, and while

---
+++ Unless,_Until,_and_While.rb
@@ -0,0 +1,20 @@
+unless condition
+  #thing to be done if the condition is false
+end
+
+while condition
+    #something to do
+end
+
+until condition
+    #something to do
+end
+
+
+# Make a while or until statement from scratch that will puts "I'm looping!" to the console 7 times.
+i = 0
+until i == 7
+  puts "I'm looping!"
+  i += 1
+end
+
