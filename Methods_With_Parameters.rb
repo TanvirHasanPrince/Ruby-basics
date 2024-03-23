0767edba8fd22a035d20248130b6086c7ca71913
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-03-23 12:23:16 +0600

    Methods with parameters

---
+++ Methods_With_Parameters.rb
@@ -0,0 +1,16 @@
+def clap_hands(number)
+    puts "Clap " * number
+end
+
+clap_hands(3)
+
+# Using the provide code, create a method called square that takes a parameter number and then puts the squared result of the number you pass through. To pass the test, square the number 2. Remember to call the method!
+
+#define the method here
+def square (number)
+ puts number * number
+end
+
+square(2)
+
+#don't forget to call the method with the argument
