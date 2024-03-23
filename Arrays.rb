b76c53b4726357e741c707285bd476fb7385bc1c
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-03-23 11:40:20 +0600

    Arrays

---
+++ Arrays.rb
@@ -0,0 +1,10 @@
+myArray = []  # an empty array
+myOtherArray = [1, 2, 3]  # an array with three elements
+
+# For changing an array
+myOtherArray[3] = 4
+
+
+# Using the provided code, print "Me!" to the console.
+myArray = ["Not me!", "Not me!", "Me!", "Not me!"]
+puts myArray[2]
