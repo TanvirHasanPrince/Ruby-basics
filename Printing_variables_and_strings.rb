a70580585d4adc68bf7d3f72faddc39541d345c4
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-03-23 11:37:52 +0600

    Printing variabls and strings

---
+++ Printing_variables_and_strings.rb
@@ -0,0 +1,19 @@
+number = 8
+puts "I have #{number} oranges." # I have 8 oranges
+
+
+first_number = 5
+second_number = 6
+
+puts "Multiplying #{first_number} by #{second_number} the result is #{first_number * second_number}"
+
+#Modifiy the code to print using the variables: - How many apples each of the kids have; - The sum of all the apples.
+
+john = 5
+mary = 3
+will = 2
+
+puts "John has #{john} apples."
+puts "Mary has #{mary} apples."
+puts "Will has #{will} apples."
+puts "Together they have #{john+mary+will} apples."
